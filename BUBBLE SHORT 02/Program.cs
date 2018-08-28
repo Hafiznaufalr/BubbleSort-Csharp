5995d5d6ea116331b8d66f6e940b33c55fd0db95
Hafiznaufalr <hafiz.nr5758@gmail.com>
2018-08-28 14:12:57 +0700

    first commit

---
+++ BUBBLE SHORT 02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace BUBBLE_SHORT_02
+{
+    class Program
+    {
+        public void BUBBLESHORT(int[] A)
+        {
+            int N = A.Length-1;
+            for (int X = 0; X < N - 1; X++)
+                for (int Y = 0; Y < N- X;Y++)
+                    if (A[Y] > A[Y+1])
+                    {
+                        int S = A[Y]; A[Y] = A[Y+1]; A[Y+1] = S;
+                    }
+        }
+        public static void Main()
+        {
+            ForegroundColor = ConsoleColor.Red;
+            int N;
+            Write("\n Jumlah Data = "); N = int.Parse(ReadLine());
+            int[] A = new int[N];
+            WriteLine("\n Unsort Numbers : ");
+
+            Random R = new Random();
+            for (int X = 0; X < N; X++)
+            {
+                A[X] = R.Next(100, 1000); Write(" {0} ", A[X]);
+            }
+
+            Program P = new Program(); P.BUBBLESHORT(A);
+            ForegroundColor = ConsoleColor.Cyan;
+            WriteLine("\n\n Sorted Numbers : ");
+            foreach (int X in A) Write(" {0} ", X);
+            ReadKey();
+
+        }
+    }
+}
+
